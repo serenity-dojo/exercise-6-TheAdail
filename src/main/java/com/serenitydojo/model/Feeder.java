974c988a30f28e467dbff3816e7f31809e929a60
Adail Retamal <adail.retamal@gmail.com>
2024-03-16 14:32:31 -0300

    My solution

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,19 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+
+        switch (animal) {
+            case "Cat":
+                return isPremium ? "Salmon": "Tuna";
+
+            case "Dog":
+                return isPremium ? "Deluxe Dog Food": "Dog Food";
+
+            case "Hamster":
+                return isPremium ? "Lettuce": "Cabbage";
+
+            default:
+                return "Food";
+        }
     }
 }
